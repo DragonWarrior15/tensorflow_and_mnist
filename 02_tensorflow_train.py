aef1a842caf3ff50b7cd70f33c6633319fa54961
Vaibhav Ojha <vaibhav.ojha@operasolutions.com>
2017-05-15 17:08:48 +0530

    complete code for a simple linear regression model

--- 02_tensorflow_train.py
+++ 02_tensorflow_train.py
@@ -16,13 +16,18 @@ y = tf.placeholder(tf.float32)
 squared_deltas = tf.square(linear_model - y)
 loss = tf.reduce_sum(squared_deltas)
 
-init = tf.global_variables_initializer()
-
 optimizer = tf.train.GradientDescentOptimizer(0.01)
+# here a single run of the optimizer just runs a single gradient decent step
+# use the for loop to run several such iterations and get the opimal parameters set
 train = optimizer.minimize(loss)
 
+sess = tf.Session()
+init = tf.global_variables_initializer()
+
 sess.run(init) # reset values to incorrect defaults.
-for i in range(1000):
-  sess.run(train, {x:[1,2,3,4], y:[0,-1,-2,-3]})
+for i in range(100):
+    sess.run(train, {x:[1,2,3,4], y:[0,-1,-2,-3]})
+    print(i, sess.run([W, b]))
 
 print(sess.run([W, b]))
+# [array([-0.9999969], dtype=float32), array([ 0.99999082], dtype=float32)]
