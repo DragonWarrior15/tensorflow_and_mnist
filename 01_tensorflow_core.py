738381c7e4a85c6c18625ae5d4873c538a1d9cb7
Vaibhav Ojha <vaibhav.ojha@operasolutions.com>
2017-05-15 15:34:25 +0530

    updated tensorflow basics file with basic node creation and computation code

--- 01_tensorflow_core.py
+++ 01_tensorflow_core.py
@@ -10,3 +10,17 @@ print(node1, node2)
 # Tensor("Const:0", shape=(), dtype=float32) Tensor("Const_1:0", shape=(), dtype=float32)
 # note that the nodes need to be evaluated in order to produce the values stored in them as the outputs
 
+# To actually evaluate the nodes, we must run the computational graph within a session.
+# A session encapsulates the control and state of the TensorFlow runtime
+
+sess = tf.Session()
+print(sess.run([node1, node2]))
+# [3.0, 4.0]
+
+# can combine the nodes through operators
+node3 = tf.add(node1, node2)
+print("node3: ", node3)
+print("sess.run(node3): ",sess.run(node3))
+# node3:  Tensor("Add:0", shape=(), dtype=float32)
+# sess.run(node3):  7.0
+
