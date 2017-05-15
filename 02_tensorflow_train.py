d67e944bbdfad581f7a0fb1e9f58b9e4d2e43f8e
Vaibhav Ojha <vaibhav.ojha@operasolutions.com>
2017-05-15 16:54:27 +0530

    initialized file for tensorflow train and optimizer apis

---
+++ 02_tensorflow_train.py
@@ -0,0 +1,28 @@
+# after completing 01_tensorflow_core.py, we jump into the tf.train API
+# for training our machine learning models
+
+# tf has inbuilt optimizers to slowly change the values of parameters
+# to "learn"
+
+import tensorflow as tf
+
+# initialize parameters to incorrect defaults
+W = tf.Variable([.3], tf.float32)
+b = tf.Variable([-.3], tf.float32)
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+
+y = tf.placeholder(tf.float32)
+squared_deltas = tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_deltas)
+
+init = tf.global_variables_initializer()
+
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+sess.run(init) # reset values to incorrect defaults.
+for i in range(1000):
+  sess.run(train, {x:[1,2,3,4], y:[0,-1,-2,-3]})
+
+print(sess.run([W, b]))
